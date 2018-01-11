57e80979fd1b11545bafa8fde45cc3384aa896d0
Bruno Carvalho <dipsnatchy26@gmail.com>
2018-01-11 12:27:04 -0600

    add image picker and fix bugs

--- containers/components/screens/Home.js
+++ containers/components/screens/Home.js
@@ -5,12 +5,15 @@ import {
   Linking,
   View,
   TouchableHighlight,
-  TextInput
+  TextInput,
+  Image,
+  Alert
 } from "react-native";
 import { Card } from "react-native-elements";
 import Button from "antd-mobile/lib/button";
 import styled from "styled-components/native";
 import axios from "axios";
+const ImagePicker = require("react-native-image-picker");
 
 import { connect } from "react-redux";
 
@@ -34,14 +37,18 @@ class Home extends Component {
     this.state = {
       medicine: [],
       create: false,
+      createButton: true,
       name: "",
-      image: "No Picture Found",
+      image:
+        "http://drpattydental.com/wp-content/uploads/2017/05/placeholder.png",
       description: "",
       rxcuis: "",
-      loaded: false
+      loaded: true,
+      uri: ""
     };
 
     this.createNewMedicine = this.createNewMedicine.bind(this);
+    this.getImage = this.getImage.bind(this);
   }
 
   componentDidMount() {
@@ -54,52 +61,86 @@ class Home extends Component {
       .catch(console.log);
   }
 
+  // CAMERA TEST
+
+  getImage = () => {
+    ImagePicker.showImagePicker({ cameraType: "back" }, response => {
+      console.log(response.uri);
+      let source = response.uri;
+      this.setState({
+        image: source
+      });
+      return true;
+    });
+  };
+
   createNewMedicine() {
     const { id } = this.props.user;
-    const { name, image, description, medicine } = this.state;
+    const { name, image, description, medicine, loaded } = this.state;
+    this.setState({ loaded: false });
     axios
       .get(`https://rxnav.nlm.nih.gov/REST/rxcui?name=${name}`)
       .then(res => {
-        this.setState({ rxcuis: res.data.idGroup.rxnormId[0] });
-        const { rxcuis } = this.state;
-        axios
-          .post("http://localhost:3005/api/createmedicine", {
-            name,
-            image,
-            description,
-            rxcuis,
-            id
-          })
-          .then(res => {
-            medicine.push(res.data);
-            this.setState({ loaded: true });
-          })
-          .catch(console.log);
+        if (res.data.idGroup.rxnormId) {
+          this.setState({ rxcuis: res.data.idGroup.rxnormId[0] });
+          const { rxcuis } = this.state;
+          axios
+            .post("http://localhost:3005/api/createmedicine", {
+              name,
+              image,
+              description,
+              rxcuis,
+              id
+            })
+            .then(res => {
+              medicine.push(res.data);
+              this.setState({ loaded: true });
+              return null;
+            })
+            .catch(console.log);
+        } else {
+          this.setState({ loaded: true });
+          Alert.alert("Medicine not found", "Make sure the name is right");
+          return null;
+        }
       })
       .catch(console.log);
   }
 
   render() {
     const { navigation, user } = this.props;
+    const { loaded, createButton } = this.state;
     return (
       <View style={{ flex: 1 }}>
-        <CreateButton onPress={() => this.setState({ create: true })}>
-          <Text>Create New</Text>
-        </CreateButton>
+        {createButton ? (
+          <CreateButton
+            onPress={() => this.setState({ create: true, createButton: false })}
+          >
+            <Text>Create New</Text>
+          </CreateButton>
+        ) : null}
         <ScrollView contentContainerStyle={{ paddingVertical: 20 }}>
           {this.state.create ? (
             <Card>
+              <Image
+                source={{ uri: this.state.image }}
+                style={{ height: 300 }}
+              />
+
+              <EditButton onPress={this.getImage} style={{ marginTop: 20 }}>
+                <Text>Get Image</Text>
+              </EditButton>
+
               <TextInput
                 onChangeText={e => this.setState({ name: e })}
                 placeholder="Name"
+                style={{ marginTop: 20 }}
               />
-              <TextInput
-                onChangeText={e => this.setState({ image: e })}
-                placeholder="Image"
-              />
+
               <TextInput
                 onChangeText={e => this.setState({ description: e })}
                 placeholder="Description"
+                style={{ marginTop: 20 }}
               />
               <View style={{ flexDirection: "row", marginTop: 20 }}>
                 <EditButton
@@ -111,7 +152,7 @@ class Home extends Component {
                     backgroundColor: "#cdc8b1"
                   }}
                   onPress={() => {
-                    this.setState({ create: false });
+                    this.setState({ create: false, createButton: true });
                   }}
                 >
                   <Text>Cancel</Text>
@@ -127,7 +168,7 @@ class Home extends Component {
                   }}
                   onPress={() => {
                     this.createNewMedicine();
-                    this.setState({ create: false });
+                    this.setState({ create: false, createButton: true });
                   }}
                 >
                   <Text>Save</Text>
@@ -135,20 +176,23 @@ class Home extends Component {
               </View>
             </Card>
           ) : null}
-          {this.state.medicine.map(({ name, image, description }, i) => (
-            <Card
-              title={name}
-              image={{ uri: image || this.state.image }}
-              key={i}
-              editable={true}
-            >
-              <Text style={{ marginBottom: 10 }}>{description}</Text>
 
-              <EditButton underlayColor={"red"} onPress={() => true}>
-                <Text style={{ fontSize: 30 }}>Edit</Text>
-              </EditButton>
-            </Card>
-          ))}
+          {loaded
+            ? this.state.medicine.map(({ name, image, description }, i) => (
+                <Card
+                  title={name}
+                  image={{ uri: image }}
+                  key={i}
+                  editable={true}
+                >
+                  <Text style={{ marginBottom: 20 }}>{description}</Text>
+
+                  <EditButton underlayColor={"red"} onPress={() => true}>
+                    <Text style={{ fontSize: 30 }}>Edit</Text>
+                  </EditButton>
+                </Card>
+              ))
+            : null}
         </ScrollView>
       </View>
     );
--- containers/components/screens/Profile.js
+++ containers/components/screens/Profile.js
@@ -1,29 +1,18 @@
 import React from "react";
-import { View } from "react-native";
+import { View, ScrollView } from "react-native";
 import { Card, Button, Text } from "react-native-elements";
 
 export default ({ navigation }) => (
   <View style={{ paddingVertical: 20 }}>
-    <Card title="John Doe">
-      <View
-        style={{
-          backgroundColor: "#bcbec1",
-          alignItems: "center",
-          justifyContent: "center",
-          width: 80,
-          height: 80,
-          borderRadius: 40,
-          alignSelf: "center",
-          marginBottom: 20
-        }}
-      >
+    <ScrollView>
+      <Card title="Alarm">
         <Text style={{ color: "white", fontSize: 28 }}>JD</Text>
-      </View>
-      <Button
-        backgroundColor="#03A9F4"
-        title="SIGN OUT"
-        onPress={() => navigation.navigate("SignedOut")}
-      />
-    </Card>
+        <Button
+          backgroundColor="#03A9F4"
+          title="SIGN OUT"
+          onPress={() => navigation.navigate("SignedOut")}
+        />
+      </Card>
+    </ScrollView>
   </View>
 );
